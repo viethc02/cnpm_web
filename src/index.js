5dd13108d0e6c736761959679e4a6e9676f3a5d7
viethc02 <viethc02@gmail.com>
2022-04-18 09:19:46 +0700

    second commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const express = require('express');
+const handlebars = require('express-handlebars');
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+//Template engine
+app.engine('hbs', handlebars.engine({
+  extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+app.get('/', (req, res) => {
+  res.render('home');
+})
+
+app.get('/news', (req, res) => {
+  res.render('home');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
